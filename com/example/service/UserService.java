58f58128f8610132505f740e7964eddf916fbacb
catCokkie <761436715@qq.com>
2018-01-31 13:06:57 +0800

    1

--- com/example/service/UserService.java
+++ com/example/service/UserService.java
@@ -2,10 +2,36 @@ package com.example.service;
 
 import com.example.role.User;
 
+/**
+ *
+ */
 public interface UserService {
+    /**
+     * 存储教师
+     * @param user 教师
+     * @return
+     */
     public User saveTeacherWithRollBack (User user);
-    public User deleteUserWithRollBack(User user);
 
+    /**
+     * 删除教师
+     * @param user 教师
+     * @return
+     */
+    public User deleteTeacherWithRollBack (User user);
 
+    /**
+     * 存储学生
+     * @param user 学生
+     * @return
+     */
+    public User saveStudentWithRollBack (User user);
+
+    /**
+     * 删除学生
+     * @param user 学生
+     * @return
+     */
+    public User deleteStudentWithRollBack (User user);
 }
 
